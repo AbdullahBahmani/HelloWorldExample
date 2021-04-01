2b5750db4c07a12170a7621d994f7ed39338464f
AbdullahBahmani <abdolahbahmani@gmail.com>
2021-04-01 18:51:57 +0200

    addedd

--- src/test/java/com/qascript/app/ApplicationTest.java
+++ src/test/java/com/qascript/app/ApplicationTest.java
@@ -11,7 +11,7 @@ public class ApplicationTest {
     public void testApplication(){
         try{
 <<<<<<< HEAD
-            Assert.assertEquals("Hello Wccccorld!", app.getMessage());
+            Assert.assertEquals("Hellofgfgfgfgd Wccccorld!", app.getMessage());
         }
         catch (Exception e){
             Assert.fail("Message is yyyyyyyy not Hello World!");
