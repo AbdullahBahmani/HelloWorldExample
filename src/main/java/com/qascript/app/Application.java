65478a101884bcd9b55a40d7826b1bb566afa84d
AbdullahBahmani <abdolahbahmani@gmail.com>
2021-04-01 19:13:02 +0200

    addedd

--- src/main/java/com/qascript/app/Application.java
+++ src/main/java/com/qascript/app/Application.java
@@ -2,11 +2,7 @@ package com.qascript.app;
 
 public class Application {
 
-<<<<<<< HEAD
-    private final String message = "Hellld Yesx Run it";
-=======
-    private final String message = "Hello World Yesx Run it";
->>>>>>> 95365e30b219f84a8530e78e50533504ef3063f8
+    private final String message = "Hello World!";
 
     public Application() {}
 
